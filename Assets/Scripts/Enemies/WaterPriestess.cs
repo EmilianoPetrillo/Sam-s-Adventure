2c62704b93a9936805062ec8f23cc7b9e0b518b0
EmilianoPetrillo <94938110+EmilianoPetrillo@users.noreply.github.com>
2022-08-10 18:59:31 +0200

    bug

--- Assets/Scripts/Enemies/WaterPriestess.cs
+++ Assets/Scripts/Enemies/WaterPriestess.cs
@@ -20,49 +20,57 @@ public class WaterPriestess : Enemy
 
     protected override void Update()
     {
-        if (deathCheck == false && animator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
-            Move();
+        if (deathCheck == false) 
+        {
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+                Move();
+
+            if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Defend"))
+            {
+                animator.SetBool("Defend", false);
+                animator.SetBool("Walk", true);
+                timer = false;
+                t = 0;
+            }
+
+            if (enemySO.HP <= enemySO.MAXHP / 3)
+            {
+                StartHealPhase();
+            }
+
+            if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Heal"))
+            {
+                animator.SetBool("Heal", false);
+                animator.SetBool("Walk", true);
+                timer = false;
+                t = 0;
+            }
+
+            if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+            {
+                animator.SetBool("Attack", false);
+                animator.SetBool("Walk", true);
+                timer = false;
+                t = 0;
+            }
+
+            if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("SpecialAttack"))
+            {
+                animator.SetBool("SpecialAttack", false);
+                animator.SetBool("Walk", true);
+                timer = false;
+                t = 0;
+            }
+        } 
 
         if (timer == true)
             t += Time.deltaTime;
 
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death") && t >= animator.GetCurrentAnimatorStateInfo(0).length)
+        if (deathCheck && t >= animator.GetCurrentAnimatorStateInfo(0).length)
         {
             UIController.Instance.CoinsUp(enemySO.coins);
             GameController.Instance.LevelUp();
             Destroy(gameObject);
-        }
-        if(t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Defend"))
-        {
-            animator.SetBool("Defend", false);
-            animator.SetBool("Walk", true);
-            timer = false;
-            t = 0;
-        }
-        if (enemySO.HP <= enemySO.MAXHP / 3)
-        {
-            StartHealPhase();
-        }
-        if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Heal"))
-        {
-            animator.SetBool("Heal", false);
-            animator.SetBool("Walk", true);
-            timer = false;
-            t = 0;
-        }
-        if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-        {
-            animator.SetBool("Attack", false);
-            animator.SetBool("Walk", true);
-            timer = false;
-            t = 0;
-        }
-        if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("SpecialAttack"))
-        {
-            animator.SetBool("SpecialAttack", false);
-            animator.SetBool("Walk", true);
-            timer = false;
-            t = 0;
         }
     }
 
