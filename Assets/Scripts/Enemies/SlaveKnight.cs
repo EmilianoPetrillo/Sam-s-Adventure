a5b9eb6f9d8d8316986c656f4f2845ec874b90cb
Filippo Pegoraro <pegorarofilippo23@gmail.com>
2022-08-22 13:43:12 +0100

    doing a prototype script for "SlaveKnight"

--- Assets/Scripts/Enemies/SlaveKnight.cs
+++ Assets/Scripts/Enemies/SlaveKnight.cs
@@ -1,114 +1,119 @@
-using System;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SlaveKnight : Enemy
 {
-    //protected override void Start()
-    //{
-    //    base.Start();
-    //    animator.GetCurrentAnimatorStateInfo(0);
-    //    timer = true;
-    //    enemySO.MAXHP = enemySO.HP;
-    //}
+    protected override void Start()
+    {
+        base.Start();
+        animator.GetCurrentAnimatorStateInfo(0);
+        timer = true;
+        enemySO.MAXHP = enemySO.HP;
+    }
 
-    //public override void TakeDamage(int damage)
-    //{
-    //    if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Pray"))
-    //        base.TakeDamage(damage);
-    //}
+    public override void TakeDamage(int damage)
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Pray"))
+            base.TakeDamage(damage);
+    }
 
-    //protected override void Update()
-    //{
-    //    if (deathCheck == false)
-    //    {
-    //        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
-    //            Move();
+    protected override void Update()
+    {
+        if (deathCheck == false)
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+                Move();
 
-    //        if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Defend"))
-    //        {
-    //            animator.SetBool("Defend", false);
-    //            animator.SetBool("Walk", true);
-    //            timer = false;
-    //            t = 0;
-    //        }
+            if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Pray"))
+            {
+                animator.SetBool("Pray", false);
+                animator.SetBool("Walk", true);
+                timer = false;
+                t = 0;
+            }
 
-    //        if (enemySO.HP <= enemySO.MAXHP / 3)
-    //        {
-    //            StartHealPhase();
-    //        }
+            if (enemySO.HP <= enemySO.MAXHP / 3)
+            {
+                StartHealPhase();
+            }
 
-    //        if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Heal"))
-    //        {
-    //            animator.SetBool("Heal", false);
-    //            animator.SetBool("Walk", true);
-    //            timer = false;
-    //            t = 0;
-    //        }
+            if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Heal"))
+            {
+                animator.SetBool("Heal", false);
+                animator.SetBool("Walk", true);
+                timer = false;
+                t = 0;
+            }
 
-    //        if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-    //        {
-    //            animator.SetBool("Attack", false);
-    //            animator.SetBool("Walk", true);
-    //            timer = false;
-    //            t = 0;
-    //        }
+            if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("AttackCombo"))
+            {
+                animator.SetBool("AttackCombo", false);
+                animator.SetBool("Walk", true);
+                timer = false;
+                t = 0;
+            }
 
-    //        if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("SpecialAttack"))
-    //        {
-    //            animator.SetBool("SpecialAttack", false);
-    //            animator.SetBool("Walk", true);
-    //            timer = false;
-    //            t = 0;
-    //        }
-    //    }
+            //if (t >= animator.GetCurrentAnimatorStateInfo(0).length && animator.GetCurrentAnimatorStateInfo(0).IsName("AttackfromAir"))
+            //{
+            //    animator.SetBool("AttackFromAir", false);
+            //    animator.SetBool("Walk", true);
+            //    timer = false;
+            //    t = 0;
+            //}
+        }
 
-    //    if (timer == true)
-    //        t += Time.deltaTime;
+        if (timer == true)
+            t += Time.deltaTime;
 
-    //    if (deathCheck && t >= animator.GetCurrentAnimatorStateInfo(0).length)
-    //    {
-    //        UIController.Instance.CoinsUp(enemySO.coins);
-    //        GameController.Instance.LevelUp();
-    //        Destroy(gameObject);
-    //    }
-    //}
+        if (deathCheck && t >= animator.GetCurrentAnimatorStateInfo(0).length)
+        {
+            UIController.Instance.CoinsUp(enemySO.coins);
+            GameController.Instance.LevelUp();
+            Destroy(gameObject);
+        }
+    }
 
-    //protected override void Move()
-    //{
-    //    if (Vector2.Distance(transform.position, Player.Instance.transform.position) > enemySO.attackRange[0])
-    //    {
-    //        transform.Translate(Vector2.left * enemySO.moveSpeed * Time.deltaTime);
-    //    }
-    //    else
-    //        Attack();
-    //}
+    protected override void Move()
+    {
+        if (Vector2.Distance(transform.position, Player.Instance.transform.position) > enemySO.attackRange[0])
+        {
+            transform.Translate(Vector2.left * enemySO.moveSpeed * Time.deltaTime);
+        }
+        else
+            Attack();
+    }
 
-    //private void StartHealPhase()
-    //{
-    //    animator.SetBool("Attack", false);
-    //    animator.SetBool("SpecialAttack", false);
-    //    animator.SetBool("Walk", false);
-    //    animator.SetBool("Heal", true);
-    //    timer = true;
-    //    t = 0;
-    //}
+    private void StartHealPhase()
+    {
+        animator.SetBool("AttackCombo", false);
+        //animator.SetBool("AttackfromAir", false);
+        animator.SetBool("Walk", false);
+        animator.SetBool("Heal", true);
+        timer = true;
+        t = 0;
+    }
 
-    //protected override void Attack()
-    //{
-    //    timer = true;
-    //    float x = Random.Range(0f, 1f);
-    //    if (x <= 0.5f)
-    //        animator.SetBool("Attack", true);
-    //    else
-    //        animator.SetBool("SpecialAttack", true);
-    //}
+    protected override void Attack()
+    {
+        timer = true;
+        float x = Random.Range(0f, 1f);
+        if (x <= 0.5f)
+            animator.SetBool("AttackCombo", true);
+        else
+            animator.SetBool("Roll", true);
+    }
 
-    //public void Heal()
-    //{
-    //    enemySO.MAXHP -= enemySO.MAXHP / 100;
-    //    if (enemySO.HP <= enemySO.MAXHP)
-    //        enemySO.HP += enemySO.HP / 20;
-    //}
+    public void Heal()
+    {
+        enemySO.MAXHP -= enemySO.MAXHP / 100;
+        if (enemySO.HP <= enemySO.MAXHP)
+            enemySO.HP += enemySO.HP / 20;
+    }
+
+    public void Jump()
+    {
+        
+    }
 }
